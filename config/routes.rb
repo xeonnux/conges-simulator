0734687116ad1e6e2f8a37e463b0c5f64cfa411a
fabrice madre <stepwiseltd@gmail.com>
2026-06-06 16:39:31 +0400

    feat: controller, views + routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :simulations, only: [ :new, :create, :show ]
+  root "simulations#new"
 end
